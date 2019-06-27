9cc01ec736040e984867b85e108c953288f777c1
민동현 <mindonghyeon890@gmail.com>
2019-06-27 23:06:37 +0900

    initial commit

---
+++ HomeDoc-Core/src/main/java/com/homedoc/core/HomeDocCoreApplication.java
@@ -0,0 +1,13 @@
+package com.homedoc.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeDocCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeDocCoreApplication.class, args);
+	}
+
+}
---
+++ HomeDoc-Core/src/test/java/com/homedoc/core/HomeDocCoreApplicationTests.java
@@ -0,0 +1,16 @@
+package com.homedoc.core;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HomeDocCoreApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
